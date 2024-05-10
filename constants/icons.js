f8201d26e43e14d2d90347029ddb6a9a83cb4b94
Harsha Nawana <harshanawana@gmail.com>
2024-05-10 19:44:05 +0530

    Favourite
    Added favourite button to videos

--- constants/icons.js
+++ constants/icons.js
@@ -11,6 +11,7 @@ import logout from "../assets/icons/logout.png";
 import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
 import play from "../assets/icons/play.png";
+import favourite from "../assets/icons/favourite.png";
 
 export default {
   play,
@@ -26,4 +27,5 @@ export default {
   logout,
   eyeHide,
   eye,
+  favourite,
 };
